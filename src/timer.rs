67d7e5aa7cf1f4a263f74ce00fb98f8d0bbaa6f1
quelledanielle <quelledanielle@users.noreply.github.com>
2018-03-01 11:15:57 -0500

    Add timer tests

--- src/timer.rs
+++ src/timer.rs
@@ -8,7 +8,7 @@ impl Timer {
     pub fn new(initial: u8) -> Timer {
         Timer {
             current: initial,
-            active: true
+            active: initial != 0
         }
     }
 
@@ -23,5 +23,67 @@ impl Timer {
 
     pub fn set(&mut self, value: u8) {
         self.current = value;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init_current_value() {
+        let val: u8 = 42;
+        let t = Timer::new(val);
+        assert_eq!(val, t.current);
+    }
+
+    #[test]
+    fn init_active() {
+        let t = Timer::new(42);
+        assert!(t.active);
+    }
+
+    #[test]
+    fn init_inactive() {
+        let t = Timer::new(0);
+        assert!(!t.active);
+    }
+
+    #[test]
+    fn tick_inactive_no_op() {
+        let mut t = Timer::new(0);
+        let current = t.current;
+        t.tick();
+
+        assert_eq!(current, t.current);
+        assert!(!t.active);
+    }
+
+    #[test]
+    fn tick_active_decrements_current() {
+        let mut t = Timer::new(42);
+        let current = t.current;
+        t.tick();
+
+        assert_eq!(current - 1, t.current);
+        assert!(t.active);
+    }
+
+    #[test]
+    fn tick_deactivates_at_zero() {
+        let mut t = Timer::new(1);
+        t.tick();
+
+        assert_eq!(0, t.current);
+        assert!(!t.active);
+    }
+
+    #[test]
+    fn set_current_value() {
+        let val: u8 = 42;
+        let mut t = Timer::new(24);
+        t.set(val);
+
+        assert_eq!(val, t.current);
     }
 }
