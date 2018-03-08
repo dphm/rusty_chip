1dedb134d000bb8a8dd5a97a614523fcdea453da
quelledanielle <quelledanielle@users.noreply.github.com>
2018-03-08 17:26:04 -0500

    Change GraphicsOutput trait to take Vec of bool

--- src/output/graphics.rs
+++ src/output/graphics.rs
@@ -4,5 +4,5 @@ const SCREEN_HEIGHT: usize = 32;
 const SCREEN_SIZE: usize = SCREEN_WIDTH * SCREEN_HEIGHT;
 
 pub trait GraphicsOutput {
-    fn display(&self, [bool; SCREEN_SIZE]);
+    fn display(&self, Vec<bool>);
 }
